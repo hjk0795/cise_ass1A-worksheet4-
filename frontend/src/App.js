e3588590795b8f3ec3f952351265991733161f6d
hjk0795 <hjk0795@gmail.com>
2022-04-08 02:35:29 +0900

    css added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import {
+  Route,
+  Routes,
+  NavLink,
+  BrowserRouter as Router,
+  Navigate
+} from "react-router-dom";
 
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
-import SubmitArticle from "./pages/Submit-Article"; 
+import SubmitArticle from "./pages/Submit-Article";
 import NotFoundPage from "./pages/404";
+
+
 
 const App = () =>  {
     return (
@@ -11,12 +20,17 @@ const App = () =>  {
         <div>
           <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
             <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+                <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
           <div className="content">
-            
+            <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route  path="/SEPractice" element={<SEPractice/>}/>
+            <Route  path="/SubmitArticle" element={<SubmitArticle/>}/>
+            <Route exact path="/404" element={<Navigate replace to="/404"/>}/>
+            </Routes>
           </div>
         </div>
         </Router>
